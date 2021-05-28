416824b636a38a539c643221aa95856d130a2e41
dongjiawen <dongjiawen@wulianshuntong.com>
2021-05-28 12:08:32 +0800

    修改首页布局

--- src/views/home/HomeIndex.vue
+++ src/views/home/HomeIndex.vue
@@ -75,7 +75,6 @@ export default defineComponent({
 
 .bottom {
   position: relative;
-  height: 200px;
 
   .footer {
     position: absolute !important;
@@ -87,7 +86,6 @@ export default defineComponent({
     position: absolute;
     bottom: 0;
     width: 100%;
-    height: 200px;
   }
 }
 
